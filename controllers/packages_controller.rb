dd170a5015d13827157fa4915b8be3f127adaab7
José Bonnet <jose.bonnet@gmail.com>
2018-05-24 11:35:38 +0100

    Temp file is not being returned: logging increased

--- controllers/packages_controller.rb
+++ controllers/packages_controller.rb
@@ -106,9 +106,10 @@ class PackagesController < ApplicationController
 
   get '/:package_uuid/files/:file_uuid?' do 
     captures=params.delete('captures') if params.key? 'captures'
-    file_name = FetchPackagesService.file_by_uuid(symbolized_hash(params))
+    file_name, file_type = FetchPackagesService.file_by_uuid(symbolized_hash(params))
     halt 404, {}, {error: ERROR_PACKAGE_FILE_NOT_FOUND % params[:package_uuid]}.to_json if file_name.to_s.empty? # covers nil
-    send_file '/tmp/'+file_name, type: 'application/octet-stream', filename: file_name
+    STDERR.puts "GET /packages/:package_uuid/files/:file_uuid: File '/tmp/#{file_name} exists #{File.exist?('/tmp/'+file_name)}"
+    send_file '/tmp/'+file_name, type: file_type, filename: file_name
   end
 
   delete '/:package_uuid/?' do 
